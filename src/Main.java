c71851291820fb60f970d0f9a0032c2f81d81623
Oscar <plasenciasandoval1304@gmail.com>
2024-02-12 21:53:39 +0100

    Punto 4 acabado

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
+
 public class Main {
 
-    Scanner input = new Scanner(System.in);
 
+    Scanner input = new Scanner(System.in);
+    public String[][] clients = new String[100][2];
+    public String[][] mecanics = new String[100][3];
+    public String[][] vehicles = new String[100][3];
+    public String[][] reparacions = new String[100][3];
 
 
     public static void main(String[] args) {
-
         Main main = new Main();
         main.init();
     }
 
 
-
-    public void init(){
+    public void init() {
         int menuItem = 0;
         do {
             System.out.println("TALLER DE REPARACIÓ DE VEHICLES");
@@ -22,44 +25,105 @@ public class Main {
             System.out.println("[2] Donar d’alta nou mecànic");
             System.out.println("[3] Introduir nou vehicle");
             System.out.println("[4] Crear fitxa de nova reparació");
-            System.out.println("[5] Sortir");
+            System.out.println("[5] Modificar reparació");
+            System.out.println("[6] Sortir");
             System.out.println("Sel·lecciona una opció: ");
 
-            if (input.hasNextInt()){
+
+            if (input.hasNextInt()) {
                 menuItem = input.nextInt();
-                switch (menuItem){
+                switch (menuItem) {
                     case 1:
                         System.out.println("Has triat donar d’alta nou client....");
-                        //insert code here
                         break;
                     case 2:
                         System.out.println("Has triat donar d’alta nou mecànic....");
-                        //insert code here
                         break;
                     case 3:
                         System.out.println("Has triat introduir nou vehicle....");
-                        //insert code here
                         break;
                     case 4:
                         System.out.println("Has triat crear fitxa de nova reparació....");
-                        //insert code here
+                        crearFitxaNovaReparacio();
                         break;
                     case 5:
+                        System.out.println("Has triat modificar reparació....");
+                        break;
+                    case 6:
                         System.out.println("Sortint....");
                         break;
                     default:
                         System.out.println("Opció no vàlida");
                 }
-            }else{
+            } else {
                 System.out.println("Opció no vàlida");
             }
-            input.nextLine();
+            input.nextLine(); // Limpiar el buffer del scanner
             System.out.println(" ");
 
-        }while(menuItem!=5);
 
+        } while (menuItem != 6);
     }
 
-    //insert code here
+    private void crearFitxaNovaReparacio() {
+        System.out.println("Introdueix la matrícula del vehicle:");
+        String matricula = input.next();
 
+
+        // Validar que exista un vehículo con la matrícula proporcionada
+        boolean vehicleExists = false;
+        for (int i = 0; i < vehicles.length; i++) {
+            if (vehicles[i][0] != null && vehicles[i][0].equalsIgnoreCase(matricula)) {
+                vehicleExists = true;
+                break;
+            }
+        }
+        if (!vehicleExists) {
+            System.out.println("No s'ha trobat cap vehicle amb aquesta matrícula.");
+            return;
+        }
+
+
+        // Mostrar lista de mecànicos disponibles
+        System.out.println("Mecànics disponibles:");
+        for (int i = 0; i < mecanics.length; i++) {
+            if (mecanics[i][0] != null && mecanics[i][2].equalsIgnoreCase("lliure")) {
+                System.out.println(mecanics[i][0] + " - " + mecanics[i][1]);
+            }
+        }
+
+
+        System.out.println("Introdueix el codi d'empleat del mecànic:");
+        String codiMecanic = input.next();
+
+
+        // Validar que el mecànic existeixi i estigui lliure
+        boolean mecanicExists = false;
+        for (int i = 0; i < mecanics.length; i++) {
+            if (mecanics[i][0] != null && mecanics[i][0].equalsIgnoreCase(codiMecanic)
+                    && mecanics[i][2].equalsIgnoreCase("lliure")) {
+                mecanicExists = true;
+                // Actualizar el estado del mecànic a "ocupat"
+                mecanics[i][2] = "ocupat";
+                break;
+            }
+        }
+        if (!mecanicExists) {
+            System.out.println("No s'ha trobat cap mecànic disponible amb aquest codi d'empleat.");
+            return;
+        }
+
+
+        // Crear la fitxa de reparació
+        for (int i = 0; i < reparacions.length; i++) {
+            if (reparacions[i][0] == null) {
+                reparacions[i][0] = matricula;
+                reparacions[i][1] = codiMecanic;
+                reparacions[i][2] = "en curs"; // L'estat inicial serà "en curs"
+                System.out.println("Fitxa de reparació creada amb èxit.");
+                return;
+            }
+        }
+        System.out.println("No es poden afegir més fitxes de reparació. S'ha assolit el límit.");
+    }
 }
